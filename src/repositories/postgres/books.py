afa58a3a9a1c01809bfc403106dfca83fe7910bf
Oleksandr Tsikhun <sasacihun4@gmail.com>
2025-04-01 11:05:20 +0300

    feat: Implement CRUD operations with validation for books including retrieval, update, and deletion

--- src/repositories/postgres/books.py
+++ src/repositories/postgres/books.py
@@ -1,7 +1,7 @@
 from sqlalchemy import text
 
-from src.book_management.models import Author, Book
-from src.repositories.postgres.repository import PostgresRepository
+from book_management.models import Author, Book
+from repositories.postgres.repository import PostgresRepository
 
 
 class BooksRepository(PostgresRepository):
