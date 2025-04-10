bfa5fbc2889b7811f7ae68f3131f3a3ba5a8c084
Oleksandr Tsikhun <sasacihun4@gmail.com>
2025-04-10 16:48:10 +0300

    fix: Create the engine globally and not with each function call

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,15 +1,20 @@
 from fastapi import Depends
+from sqlalchemy.exc import ArgumentError
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from auth.services import get_active_current_user, oauth2_scheme
 from config import settings
+from repositories.postgres.container import PostgresUnitOfWork
+
+# In order not to create an engine every time I call get_unit_of_work,
+# I call it this way and catch the error of missing `DATABASE_URL`
+try:
+    engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
+except ArgumentError:
+    print("ArgumentError")
 
 
 def get_unit_of_work():
-    from repositories.postgres.container import PostgresUnitOfWork
-
-    engine = create_async_engine(settings.DATABASE_URL, echo=True, future=True)
-
     return PostgresUnitOfWork(engine)
 
 
